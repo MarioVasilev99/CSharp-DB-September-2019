f490e26cd6091816c2208156e52fb096a447cca8
Mario Vasilev <41074548+MarioVasilev99@users.noreply.github.com>
2019-11-07 20:55:11 +0200

    Added Hospital Database

---
+++ Entity Framework Core/Code First/HospitalDatabase/HospitalDatabase/Data/HospitalContext.cs
@@ -0,0 +1,44 @@
+﻿namespace P01_HospitalDatabase.Data
+{
+    using P01_HospitalDatabase.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class HospitalContext : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+
+        public DbSet<Medicament> Medicaments { get; set; }
+
+        public DbSet<Diagnose> Diagnoses { get; set; }
+
+        public DbSet<Visitation> Visitations { get; set; }
+
+        public DbSet<PatientMedicament> PatientMedicaments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(DataSettings.DefaultConnection);
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Patient>()
+                .HasMany(p => p.Prescriptions)
+                .WithOne(pr => pr.Patient)
+                .HasForeignKey(p => p.PatientId);
+
+            modelBuilder
+                .Entity<Medicament>()
+                .HasMany(m => m.Prescriptions)
+                .WithOne(pr => pr.Medicament)
+                .HasForeignKey(m => m.MedicamentId);
+
+            modelBuilder
+                .Entity<PatientMedicament>()
+                .HasKey(pk => new { pk.PatientId, pk.MedicamentId });
+        }
+    }
+}
