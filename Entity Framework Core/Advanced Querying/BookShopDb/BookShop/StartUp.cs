78fb5fcd7d76c265489d4e351e83cbfe3f919dbc
Mario Vasilev <41074548+MarioVasilev99@users.noreply.github.com>
2019-11-16 16:32:38 +0200

    Added Some Homework Problem Solutions

--- Entity Framework Core/Advanced Querying/BookShopDb/BookShop/StartUp.cs
+++ Entity Framework Core/Advanced Querying/BookShopDb/BookShop/StartUp.cs
@@ -14,7 +14,7 @@
             {
                 var userCommand = Console.ReadLine();
 
-                var result = GetGoldenBooks(db);
+                var result = GetBookTitlesContaining(db, userCommand);
                 Console.WriteLine(result);
             }
         }
@@ -54,6 +54,118 @@
                 .ForEach(b => resultSb.AppendLine(b.Title));
 
             return resultSb.ToString().TrimEnd();
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var books = context
+                .Books
+                .Where(b => b.Price > 40)
+                .Select(b => new { b.Title, b.Price })
+                .OrderByDescending(b => b.Price)
+                .ToList();
+
+            var resultSb = new StringBuilder();
+
+            foreach (var book in books)
+            {
+                var lineToAppend = $"{book.Title} - ${book.Price:F2}";
+
+                resultSb.AppendLine(lineToAppend);
+            }
+
+            return resultSb.ToString().TrimEnd();
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            var books = context
+                .Books
+                .Where(b => b.ReleaseDate.Value.Year != year)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToList();
+
+            return string.Join(Environment.NewLine, books);
+        }
+
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            var categories = input.Split(" ").Select(c => c.ToLower()).ToList();
+
+            var books = context
+                .Books
+                .Where(b => b.BookCategories
+                             .Any(bc => categories.Contains(bc.Category.Name.ToLower())))
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title)
+                .ToList();
+
+            return string.Join(Environment.NewLine, books);
+        }
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            var maxReleaseDate = DateTime.ParseExact(date, "dd-MM-yyyy", null);
+
+            var books = context
+                .Books
+                .Where(b => b.ReleaseDate < maxReleaseDate)
+                .OrderByDescending(b => b.ReleaseDate)
+                .Select(b => new
+                {
+                    b.Title,
+                    b.Price,
+                    b.EditionType
+                })
+                .ToList();
+
+            var resultSb = new StringBuilder();
+
+            foreach (var book in books)
+            {
+                var bookPresenationLine = $"{book.Title} - {book.EditionType} - ${book.Price:F2}";
+
+                resultSb.AppendLine(bookPresenationLine);
+            }
+
+            return resultSb.ToString().TrimEnd();
+        }
+
+        public static string GetAuthorNamesEndingIn(BookShopContext context, string input)
+        {
+            var authors = context
+                .Authors
+                .Where(a => a.FirstName.EndsWith(input))
+                .Select(a => new
+                {
+                    FullName = $"{a.FirstName} {a.LastName}"
+                })
+                .OrderBy(a => a.FullName)
+                .ToList();
+
+            var resultSb = new StringBuilder();
+
+            foreach (var author in authors)
+            {
+                resultSb.AppendLine(author.FullName);
+            }
+
+            return resultSb.ToString().TrimEnd();
+        }
+
+        public static string GetBookTitlesContaining(BookShopContext context, string input)
+        {
+            var stringToContain = input.ToLower();
+
+            var books = context
+                .Books
+                .Where(b => b.Title.ToLower().Contains(stringToContain))
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title)
+                .ToList();
+
+            return string.Join(Environment.NewLine, books);
         }
     }
 }
