b4ab0b82ee7aa28e6ce36853954eda2500b96c67
Mario Vasilev <41074548+MarioVasilev99@users.noreply.github.com>
2019-11-16 15:41:44 +0200

    Uploaded part of the Advanced Querying Homework

---
+++ Entity Framework Core/Advanced Querying/BookShopDb/BookShop/StartUp.cs
@@ -0,0 +1,59 @@
+﻿namespace BookShop
+{
+    using Data;
+
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                var userCommand = Console.ReadLine();
+
+                var result = GetGoldenBooks(db);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var bookTitles = context
+                .Books
+                .Where(b => b.AgeRestriction.ToString().ToLower() == command.ToLower())
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title)
+                .ToList();
+
+            var resultSb = new StringBuilder();
+
+            foreach (var title in bookTitles)
+            {
+                resultSb.AppendLine(title);
+            }
+
+            return resultSb.ToString().TrimEnd();
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var goldenBooks = context
+                .Books
+                .Where(b => b.EditionType.ToString() == "Gold")
+                .Where(b => b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => new { b.Title })
+                .ToList();
+
+            var resultSb = new StringBuilder();
+
+            goldenBooks
+                .ForEach(b => resultSb.AppendLine(b.Title));
+
+            return resultSb.ToString().TrimEnd();
+        }
+    }
+}
