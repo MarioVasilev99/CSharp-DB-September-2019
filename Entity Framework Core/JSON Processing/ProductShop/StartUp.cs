1a1fad7f3c2f973a9efcaace93cb5d94307ca7c0
Mario Vasilev <41074548+MarioVasilev99@users.noreply.github.com>
2019-11-20 22:58:30 +0200

    Added Problem Descriptions and Imported Users and Products

---
+++ Entity Framework Core/JSON Processing/ProductShop/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace ProductShop
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    using Newtonsoft.Json;
+
+    using ProductShop.Data;
+    using ProductShop.Models;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new ProductShopContext())
+            {
+                var jsonPath = "./../../../Datasets/products.json";
+                var json = File.ReadAllText(jsonPath);
+
+                var result = ImportProducts(db, json);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            var products = JsonConvert.DeserializeObject<Product[]>(inputJson);
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Length}";
+        }
+    }
+}
