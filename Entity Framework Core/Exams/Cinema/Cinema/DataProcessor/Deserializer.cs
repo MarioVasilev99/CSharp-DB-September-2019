5993b2fb525e3b194107cced698135cb9a6f3151
Mario Vasilev <41074548+MarioVasilev99@users.noreply.github.com>
2019-12-04 19:01:09 +0200

    Added Part of the Cinema Exam Solution

---
+++ Entity Framework Core/Exams/Cinema/Cinema/DataProcessor/Deserializer.cs
@@ -0,0 +1,92 @@
+﻿namespace Cinema.DataProcessor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using Cinema.Data.Models;
+    using Cinema.Data.Models.Enums;
+    using Cinema.DataProcessor.ImportDto;
+    using Data;
+    using Newtonsoft.Json;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+        private const string SuccessfulImportMovie 
+            = "Successfully imported {0} with genre {1} and rating {2}!";
+        private const string SuccessfulImportHallSeat 
+            = "Successfully imported {0}({1}) with {2} seats!";
+        private const string SuccessfulImportProjection 
+            = "Successfully imported projection {0} on {1}!";
+        private const string SuccessfulImportCustomerTicket 
+            = "Successfully imported customer {0} {1} with bought tickets: {2}!";
+
+        public static string ImportMovies(CinemaContext context, string jsonString)
+        {
+            var moviesDtos = JsonConvert.DeserializeObject<ImportMovieDto[]>(jsonString);
+            var resultSb = new StringBuilder();
+            var validMovies = new List<Movie>();
+
+            foreach (var movieDto in moviesDtos)
+            {
+                if (!IsValid(movieDto) ||
+                    context.Movies.Any(m => m.Title == movieDto.Title))
+                {
+                    resultSb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                var movie = new Movie()
+                {
+                    Title = movieDto.Title,
+                    Rating = movieDto.Rating,
+                    Director = movieDto.Director,
+                    Duration = TimeSpan.Parse(movieDto.Duration),
+                    Genre = (Genre)Enum.Parse(typeof(Genre), movieDto.Genre),
+                };
+
+                validMovies.Add(movie);
+                resultSb.AppendLine(string.Format(SuccessfulImportMovie,
+                                    movieDto.Title,
+                                    movieDto.Genre,
+                                    movieDto.Rating.ToString("F2")));
+            }
+
+            context.Movies.AddRange(validMovies);
+            context.SaveChanges();
+
+            var result = resultSb.ToString().TrimEnd();
+            return result;
+        }
+
+        public static string ImportHallSeats(CinemaContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportProjections(CinemaContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportCustomerTickets(CinemaContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsValid(object entity)
+        {
+            var validationContext = new ValidationContext(entity);
+            var validationResult = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(entity,
+                                                       validationContext,
+                                                       validationResult,
+                                                       true);
+
+            return isValid;
+        }
+    }
+}
